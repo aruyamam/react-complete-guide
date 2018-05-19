727659d3061d40d110f094c35cf79ce052dbc9ef
aruyamam <aruyamam@gmail.com>
2018-05-19 10:58:10 +0900

    Section 7 Understanding Higher Order Component (HOCs)

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import classes from './Cockpit.css';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/ReactAux';
 
 const cockpit = (props) => {
   const assignedClasses = [];
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 // import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
 
 class App extends PureComponent {
@@ -107,7 +108,7 @@ class App extends PureComponent {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => {this.setState({ showPersons: true })}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
@@ -115,7 +116,7 @@ class App extends PureComponent {
           persons={this.state.persons}
           clicked={this.togglePersonHandler} />
         {persons}
-      </div>
+      </WithClass >
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'does this work now?'));
   }
